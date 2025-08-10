0e4504177ad8a4f3ff2cdac164f90cc33e27cd77
Amodio Pesce <pscamodio@gmail.com>
2025-08-10 14:10:12 +0200

    chore: Add support for clang-tidy

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,18 @@
+# clang-tidy.cmake - Add a target to run clang-tidy on all C/C++ files in the project
+find_program(CLANG_TIDY_BIN clang-tidy)
+
+file(GLOB_RECURSE ALL_CLANG_TIDY_FILES
+    "${CMAKE_SOURCE_DIR}/engine/include/*.h"
+    "${CMAKE_SOURCE_DIR}/engine/src/*.cpp"
+    "${CMAKE_SOURCE_DIR}/game/src/*.cpp"
+)
+
+if(CLANG_TIDY_BIN)
+    add_custom_target(clang-tidy
+        COMMAND ${CLANG_TIDY_BIN} ${ALL_CLANG_TIDY_FILES} -- -I${CMAKE_SOURCE_DIR}/engine/include -I${CMAKE_SOURCE_DIR}/engine/src -I${CMAKE_SOURCE_DIR}/game/src
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMENT "Running clang-tidy on all C/C++ files"
+    )
+else()
+    message(WARNING "clang-tidy not found! Please install it to enable the run-clang-tidy target.")
+endif()
